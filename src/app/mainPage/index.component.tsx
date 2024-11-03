6ebd950f03cdb38c553eb8e812cf0877de1b36ab
Mehdi Fadaie <mehdi.f@BRCO.IT>
2024-11-03 10:16:01 +0330

    2line

--- src/app/mainPage/index.component.tsx
+++ src/app/mainPage/index.component.tsx
@@ -11,6 +11,9 @@ const MainPage = () => {
       <p>hello</p>
       {isPending && <p>helloWorl</p>}
       {data?.at(0)?.description}
+      {data?.at(0)?.title}
+      {data?.at(0)?.title}
+
     </div>
   );
 };
